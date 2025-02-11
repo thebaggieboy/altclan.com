32896705d4436d26c84d03d3d3e28af8cce5e3ec
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-02-11 12:37:22 +0100

    checkout forms error solved

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -21,7 +21,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
  
 export async function getServerSideProps(context) {
   const res = await fetch(
-    `https://altclan-brands-api-1-1.onrender.com/api/merchandises`
+    `https://altclan-brands-api-1-1.onrender.com/api/merchandises/`
   );
   //const res = await fetch(`http://127.0.0.1:8000/api/merchandises/₦{id}`);
   const data = await res.json();
@@ -50,10 +50,10 @@ export default function Checkout({ merchs }) {
   const name = firstName + " " + lastName
   const [searchQuery, setSearchQuery] = useState('');
   const [results, setResults] = useState([]);
-  const [city, setcity] = useState('')
+  const [city, setCity] = useState('')
   const [state, setstate] = useState('')
-  const [address, setaddress] = useState('')
-  const [zip, setzip] = useState('')
+  const [address, setAddress] = useState('')
+  const [zip, setZip] = useState('')
   const cartItems = useSelector(selectCartItems);
   const total = useSelector(selectCartTotal)
   const cartQuantity = useSelector(selectCartCount)
@@ -77,18 +77,17 @@ export default function Checkout({ merchs }) {
 
   const ref = randomAlphaNumeric(16);
 
+    function checkoutSuccess(){
+      
+    }
+    function orderSuccess(){
+      
+    }
+
+
   const makePayment = () => {
     console.log("Payment button clicked")
     // get paystack_charge_id
-
-    updateFn({
-      user_email: email,
-      address: user?.[0]?.billing_address + "" + user?.[0]?.city + "" + user?.[0]?.state + "" + + user?.[0]?.zip,
-      paystack_charge_id: "",
-      full_name: name,
-      amount: amount / 100,
-      status: "C",
-    })
   }
   const { isPending: useOrderPending, error: useOrderError, mutateAsync: orderFn, data: useOrderData } = useOrder('https://altclan-api.onrender.com/api/order/', orderSuccess, USER_TYPES.user)
 
@@ -115,35 +114,22 @@ export default function Checkout({ merchs }) {
   }
   }
   
-  const getOrder = async()=>{
-    console.log("Getting orders from api")
-    const orderUrl = await fetch("https://altclan-api.onrender.com/api/orders/")
-
-    
-    const data = await orderUrl.json()
-    console.log("User Orders: ", data)
-    const orderResult = data?.filter((product) => product.user_email.toLowerCase().includes(email.toLowerCase()) );
-    setResults(orderResult)
-    console.log("Get Order by email: ", orderResult)
-
-    if(results.length < 1){
-      console.log(results)
-      console.log("No current orders")
-    }
-  }
-
+ 
   const componentProps = {
     email,
     amount,
     metadata: {
       name,
       phone,
+      city,
+      state,
+      zip,
       ref,
     },
     publicKey,
     text: "Pay Now",
     onSuccess: () => {
-      makePayment()
+  
       createOrder()
       
 
@@ -345,7 +331,7 @@ export default function Checkout({ merchs }) {
                 </div>
                 <label htmlFor="first-name" className="mt-4 mb-2 block text-sm font-medium">First Name</label>
                 <div className="relative">
-                  <input type="text"  onChange={(e) => setfirstName(e.target.value)} id="first_name" name="first_name" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your first name here" />
+                  <input type="text" value={first_name}   onChange={(e) => setfirstName(e.target.value)} id="first_name" name="first_name" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your first name here" />
                   <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
@@ -355,7 +341,7 @@ export default function Checkout({ merchs }) {
 
                 <label htmlFor="last-name" className="mt-4 mb-2 block text-sm font-medium">Last Name</label>
                 <div className="relative">
-                  <input type="text"  onChange={(e) => setlastName(e.target.value)} id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your last name here" />
+                  <input type="text"  value={last_name}  onChange={(e) => setlastName(e.target.value)} id="last_name" name="last_name" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your last name here" />
                   <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
@@ -365,22 +351,22 @@ export default function Checkout({ merchs }) {
             
                 <label htmlFor="address" className="mt-4 mb-2 block text-sm font-semibold">Billing Address</label>
                 <div className="relative">
-                  <input type="text"  onChange={(e) => setfirstName(e.target.value)} id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="1234 AltRd Example St, 27382" />
+                  <input type="text" value={user?.[0]?.billing_address}  onChange={(e) => setAddress(e.target.value)} id="billing_address" name="billing_address" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="1234 AltRd Example St, 27382" />
                
                 </div>
                 <label htmlFor="state" className="mt-4 mb-2 block text-sm font-medium">State</label>
                 <div className="relative">
-                  <input type="text"  onChange={(e) => setfirstName(e.target.value)} id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Lagos" />
+                  <input type="text" value={user?.[0]?.state} onChange={(e) => setstate(e.target.value)} id="state" name="state" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Lagos" />
                
                 </div>
                 <label htmlFor="city" className="mt-4 mb-2 block text-sm font-medium">City</label>
                 <div className="relative">
-                  <input type="text"  onChange={(e) => setfirstName(e.target.value)} id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Victoria Island" />
+                  <input type="text" value={user?.[0]?.city} onChange={(e) => setCity(e.target.value)} id="city" name="city" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Victoria Island" />
                
                 </div>
                 <label htmlFor="first-name" className="mt-4 mb-2 block text-sm font-medium">Zip</label>
                 <div className="relative">
-                  <input type="text"  onChange={(e) => setfirstName(e.target.value)} id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="444222" />
+                  <input type="text" value={user?.[0]?.zip} onChange={(e) => setZip(e.target.value)} id="zip" name="zip" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="444222" />
                
                 </div>
                 
